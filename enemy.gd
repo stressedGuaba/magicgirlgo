98edea6208bb79b8b0398217f80afa9c609a7e17
unknown <pawletteGC@gmail.com>
2025-09-23 20:34:22 -0700

    started base of game

---
+++ enemy.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+@onready var player = get_node("/root/Game/Player")
+
+func _physics_process(_delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 300.0
+	move_and_slide()
