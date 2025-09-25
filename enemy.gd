33f94f2ce77676eb6c7a8b6d6916f59cf5625d88
unknown <pawletteGC@gmail.com>
2025-09-25 13:52:27 -0700

    base of game mostly done ? health bar issue currently need to fix

--- enemy.gd
+++ enemy.gd
@@ -1,8 +1,17 @@
 extends CharacterBody2D
 
+var health = 3
+
 @onready var player = get_node("/root/Game/Player")
 
 func _physics_process(_delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 300.0
 	move_and_slide()
+
+func take_damage():
+	health -= 1
+	
+	if health == 0: 
+		queue_free()
+	
