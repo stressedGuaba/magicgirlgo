e32c6ef779f4e9c6b5db8eee37570b0a80069a31
unknown <pawletteGC@gmail.com>
2025-10-11 18:03:48 -0700

    ui is broken rn, saving progress just in case I break it even more

---
+++ tscn/main_menu.gd
@@ -0,0 +1,42 @@
+extends Control
+
+func _ready():
+	menu()
+
+func _on_upgrades_pressed() -> void:
+	skill_tree()
+
+func _on_atlas_pressed() -> void:
+	Atlas()
+
+func menu():
+	$Menu.show()
+	$SkillTree.hide()
+	$Atlas.hide()
+	$Gold.hide()
+	$Back.hide()
+	tween_pop($Menu)
+
+func skill_tree():
+	$SkillTree.show()
+	$Gold.show()
+	$Menu.hide()
+	$Back.show()
+	tween_pop($SkillTree)
+	
+func Atlas():
+	$Atlas.show()
+	$Menu.hide()
+	$Gold.hide()
+	$Back.show()
+	tween_pop($Atlas)
+
+func _on_back_pressed() -> void:
+	menu()
+
+func tween_pop(panel):
+	## add sound later!
+	## SoundManager.play_sfx(load())
+	panel.scale = Vector2(0.85, 0.85)
+	var tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(panel, "scale", Vector2(1, 1), 0.5)
